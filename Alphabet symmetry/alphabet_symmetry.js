1f262411cb667b9a4cfbf8b1a7ff33adeeeb7c90
annakulanna <63148334+annakulanna@users.noreply.github.com>
2021-04-10 22:50:31 +0300

    Create alphabet_symmetry.js

---
+++ Alphabet symmetry/alphabet_symmetry.js
@@ -0,0 +1,24 @@
+const alphabet = {
+  1: 'a', 2: 'b', 3: 'c',  
+  4: 'd', 5: 'e', 6: 'f',
+  7: 'g', 8: 'h', 9: 'i',
+  10: 'j', 11: 'k', 12: 'l',
+  13: 'm', 14: 'n', 15: 'o',
+  16: 'p', 17: 'q', 18: 'r',
+  19: 's', 20: 't', 21: 'u',
+  22: 'v', 23: 'w', 24: 'x',
+  25: 'y', 26: 'z'
+}
+
+const solve = (arr) => {  
+  return arr.map(word => {
+    let charArr = word.split('')
+    let number = 0;
+    for (let i = 0; i < charArr.length; i++) {
+      if (charArr[i].toLowerCase() === alphabet[i+1]){
+        number++
+      }
+    } 
+    return number
+  })
+}
